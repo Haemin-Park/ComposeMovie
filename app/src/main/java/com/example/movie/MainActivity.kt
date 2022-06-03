ae17185b2991b12bcec805c9d64e8dd329d4e0ac
박해민 <phm9368@sookmyung.ac.kr>
2022-06-03 15:50:16 +0900

    프리뷰 함수 수정

--- app/src/main/java/com/example/movie/MainActivity.kt
+++ app/src/main/java/com/example/movie/MainActivity.kt
@@ -3,36 +3,12 @@ package com.example.movie
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
-import com.example.movie.model.fakeFilmData
-import com.example.movie.ui.film.FilmDetailScreen
-import com.example.movie.ui.film.FilmItemView
-import com.example.movie.ui.film.FilmsContent
-import com.example.movie.ui.film.FilmViewModel
-import com.example.movie.ui.theme.MovieTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MovieApp()
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MovieTheme {
-        FilmItemView(film = fakeFilmData) {
-
         }
     }
 }
--- app/src/main/java/com/example/movie/ui/film/FilmDetailScreen.kt
+++ app/src/main/java/com/example/movie/ui/film/FilmDetailScreen.kt
@@ -2,13 +2,19 @@ package com.example.movie.ui.film
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.example.movie.R
 import com.example.movie.model.Film
+import com.example.movie.ui.theme.MovieTheme
 
 @Composable
 fun FilmDetailScreen(film: Film) {
@@ -16,13 +22,25 @@ fun FilmDetailScreen(film: Film) {
         model = ImageRequest.Builder(LocalContext.current)
             .placeholder(R.drawable.placeholder)
             .data(film.movieBanner)
-            .crossfade(true)
             .build()
     )
 
     Column() {
-        Image(painter = painter, contentDescription = null)
+        Image(
+            painter = painter,
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
         Text(text = film.title)
         Text(text = film.director)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FilmDetailScreenPreview(@PreviewParameter(FilmItemViewPreviewParameterProvider::class) film: Film) {
+    MovieTheme {
+        FilmDetailScreen(film = film)
     }
 }
